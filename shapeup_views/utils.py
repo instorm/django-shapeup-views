30c63f05fd787f23327d0621783a3e64ee0b41c3
Makoto Yamagata <yamagata@clouds-inc.jp>
2017-08-01 11:02:32 +0900

    change parameter name

--- shapeup_views/utils.py
+++ shapeup_views/utils.py
@@ -1,11 +1,11 @@
 def build_query_interface(seed):
 
-    def query_interface(category, selector, *, append_self=False):
+    def query_interface(category, selector, *, wrap=None):
         category = seed.get(category, None)
         interface = category.get(selector, None) if category else None
         def method_interface(self, **kwargs):
             return interface(**kwargs)
-        return method_interface if append_self else interface
+        return method_interface if wrap=='method' else interface
 
     return query_interface
 
